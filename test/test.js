df8a760414e8c616f97bbd4dd8eddd8d5c6c6d27
Wings <pgw00k@outlook.com>
2024-09-23 17:45:32 +0800

    更新加载逻辑

--- test/test.js
+++ test/test.js
@@ -1,7 +1,8 @@
-include('../FridaScript/BaseHook.js');
+import '../FridaScript/BaseHook'
 
 
 var GameAssemblyIsLoaded = false;
 var BaseModule = new ModuleHook('GameAssembly.dll');
 
 console.log('Test');
+console.log('Test2');
